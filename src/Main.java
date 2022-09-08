60b599f4642c4cfc168eb830a85cbfd92ecaf903
ImKunYoung <lim8667@gmail.com>
2022-09-08 20:34:53 +0900

    Constructor 생성

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
 
+    /*Constructor - 클래스 초기화*/
+    public Main() {
+        System.out.println("Constructor");
+    }
+
+    public void calculate() {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        int temp = 1;
+        System.out.println("temp: "+temp);
+
+        String a = "문자입니다";
+        System.out.println(a);
+
+        Main main = new Main();
+
+
     }
 
 }
