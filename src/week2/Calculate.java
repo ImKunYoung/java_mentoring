5483b490ce32ed4bd98f2046b4757ab757319b6b
ImKunYoung <lim8667@gmail.com>
2022-09-17 16:12:20 +0900

    ..

--- src/week2/Calculate.java
+++ src/week2/Calculate.java
@@ -5,37 +5,43 @@ import java.util.Scanner;
 
 public class Calculate {
 
-    // input 문자열 띄어내기
-    // String 으로 입력받은 문자열을 정수형 (int) 타입으로 변환하기
-    // 숫자, 문자열인지 파악
-    // *, -, /, + 수식을 인식하여 계산하기
-    // 계산된 결괏값을 출력하기
+    // 1. 문자열을 입력받는다
+    // 2. input 문자열 띄어내기
+    // 3. String 으로 입력받은 문자열을 정수형 (int) 타입으로 변환하기
+    // 4. 숫자, 문자열인지 파악
+    // 5. *, -, /, + 수식을 인식하여 계산하기
+    // 6. 계산된 결괏값을 출력하기
 
     public Calculate() {
         String input = this.write();
         System.out.println(input);
 
-        ArrayList<String> inputs = this.toChar(input);
+        ArrayList<Character> inputs = this.toChar(input);
         System.out.println(inputs);
 
         this.seize(inputs);
 
     }
 
-    /*숫자인지 문자열인지 파악한다*/
-    private void seize(ArrayList<String> inputs) {
+    /*4. 숫자, 문자열인지 파악*/
+    private void seize(ArrayList<Character> inputs) {
         System.out.println("/*숫자인지 문자열인지 파악한다*/");
 
         for(int i=0; i < inputs.size(); i++) {
 
-            System.out.println(inputs.get(i));
+            char input = inputs.get(i);
+
+
+            int ascii = (int) input;
+            System.out.println(ascii);
+
 
 
         }
 
     }
 
-
+    /*1. 문자열을 입력받는다*/
     private String write() {
         System.out.println("값을 입력하세요!: ");
         Scanner scanner = new Scanner(System.in);
@@ -43,15 +49,16 @@ public class Calculate {
         return input;
     }
 
-    private ArrayList<String> toChar(String input) {
-        ArrayList<String> inputs = new ArrayList<>();
+    /**/
+    private ArrayList<Character> toChar(String input) {
+        ArrayList<Character> inputs = new ArrayList<>();
 
         for(int i=0; i < input.length(); i++) {
 
             System.out.println(input.charAt(i));
 
             // char -> String 형 변환
-            inputs.add(String.valueOf(input.charAt(i)));
+            inputs.add(input.charAt(i));
 
         }
 
